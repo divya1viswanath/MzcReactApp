710a2a39d352014a54fa16701bb5b65947cf4040
divya1viswanath <divyarugmini798@gmail.com>
2022-05-13 15:39:30 +0530

    mzc react app created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col col-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">User name</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="col col col-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">Password</label>
+                    <input type="password" name="" id="" className="form-control"/>
+                </div>
+                <div className="col col col-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button className="btn btn-primary">LOGIN</button>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
   );
 }
 
